c394025c49b676806591d71d2da79e97c383582a
Caroline T. Ribeiro <caroltr.srs@gmail.com>
2017-12-23 23:11:26 -0200

    fix to build

--- realm-example/app/src/main/java/br/com/cten/tasksamplerealm/TaskListActivity.java
+++ realm-example/app/src/main/java/br/com/cten/tasksamplerealm/TaskListActivity.java
@@ -1,15 +1,11 @@
 package br.com.cten.tasksamplerealm;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -33,64 +29,84 @@ public class TaskListActivity extends AppCompatActivity {
         // RealmResults are "live" views, that are automatically kept up to date, even when changes happen
         // on a background thread. The RealmBaseAdapter will automatically keep track of changes and will
         // automatically refresh when a change is detected.
-        RealmResults<Task> tasks = realm.where(Task.class).findAll();
+        RealmResults<Task> tasks = getTasks();
         final TaskAdapter adapter = new TaskAdapter(this, tasks);
 
         ListView listView = findViewById(R.id.task_list);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
                 final Task task = (Task) adapterView.getAdapter().getItem(i);
                 final EditText taskEditText = new EditText(TaskListActivity.this);
                 taskEditText.setText(task.getName());
                 AlertDialog dialog = new AlertDialog.Builder(TaskListActivity.this)
                         .setTitle("Edit Task")
                         .setView(taskEditText)
-                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
+                        .setPositiveButton("Save", (dialogInterface, j) -> {
                                 changeTaskName(task.getId(), String.valueOf(taskEditText.getText()));
-                            }
                         })
-                        .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                deleteTask(task.getId());
-                            }
+                        .setNegativeButton("Delete", (dialogInterface, k) -> {
+                            deleteTask(task.getId());
                         })
                         .create();
                 dialog.show();
-            }
         });
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(view -> {
                 final EditText taskEditText = new EditText(TaskListActivity.this);
                 AlertDialog dialog = new AlertDialog.Builder(TaskListActivity.this)
                         .setTitle("Add Task")
                         .setView(taskEditText)
-                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                realm.executeTransactionAsync(new Realm.Transaction() {
-                                    @Override
-                                    public void execute(Realm realm) {
-                                        realm.createObject(Task.class, UUID.randomUUID().toString())
-                                                .setName(String.valueOf(taskEditText.getText()));
-                                    }
-                                });
-                            }
+                        .setPositiveButton("Add", (dialogInterface, i) -> {
+                                createTask(String.valueOf(taskEditText.getText()));
                         })
                         .setNegativeButton("Cancel", null)
                         .create();
                 dialog.show();
-            }
+        });
+    }
+
+    private void createTask(String taskName) {
+        realm.executeTransactionAsync(r -> {
+                r.createObject(Task.class, UUID.randomUUID().toString())
+                        .setName(taskName);
+        });
+    }
+
+    private RealmResults<Task> getTasks() {
+        return realm.where(Task.class).findAll();
+    }
+
+    public void changeTaskDone(final String taskId) {
+        realm.executeTransactionAsync(r -> {
+                Task task = r.where(Task.class).equalTo("id", taskId).findFirst();
+                task.setDone(!task.isDone());
+        });
+    }
+
+    private void changeTaskName(final String taskId, final String name) {
+        realm.executeTransactionAsync(r -> {
+                Task task = r.where(Task.class).equalTo("id", taskId).findFirst();
+                task.setName(name);
+        });
+    }
+
+    private void deleteTask(final String taskId) {
+        realm.executeTransactionAsync(r -> {
+                r.where(Task.class).equalTo("id", taskId)
+                        .findFirst()
+                        .deleteFromRealm();
+        });
+    }
+
+    private void deleteAllDone() {
+        realm.executeTransactionAsync(r -> {
+                realm.where(Task.class).equalTo("done", true)
+                        .findAll()
+                        .deleteAllFromRealm();
         });
     }
 
@@ -100,56 +116,11 @@ public class TaskListActivity extends AppCompatActivity {
         realm.close();
     }
 
-    public void changeTaskDone(final String taskId) {
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                Task task = realm.where(Task.class).equalTo("id", taskId).findFirst();
-                task.setDone(!task.isDone());
-            }
-        });
-    }
-
-    private void changeTaskName(final String taskId, final String name) {
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                Task task = realm.where(Task.class).equalTo("id", taskId).findFirst();
-                task.setName(name);
-            }
-        });
-    }
-
-    private void deleteTask(final String taskId) {
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                realm.where(Task.class).equalTo("id", taskId)
-                        .findFirst()
-                        .deleteFromRealm();
-            }
-        });
-    }
-
-    private void deleteAllDone() {
-        realm.executeTransactionAsync(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                realm.where(Task.class).equalTo("done", true)
-                        .findAll()
-                        .deleteAllFromRealm();
-            }
-        });
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_delete) {
             deleteAllDone();
             return true;
